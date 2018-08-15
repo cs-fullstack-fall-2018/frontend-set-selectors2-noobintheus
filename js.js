14e8008a29a88403a3abb154b37b068694e57896
student <you@example.com>
2018-08-15 13:36:52 -0500

    check in

---
+++ js.js
@@ -0,0 +1,14 @@
+document.querySelector(".example");
+console.log("fun");
+document.querySelector("#first").innerHTML = "Hello World!";
+console.log("times");
+document.getElementsByTagName("name");
+console.log("bleh");
+
+var matches = document.querySelectorAll("p")
+var x = document.getElementById("myDIV");
+x.querySelector(".example").innerHTML = "Hello World!";
+
+
+document.querySelector("h1").style.backgroundColor = "blue";
+document.querySelector("goodbye").style.backgroundColor = "yellow";
