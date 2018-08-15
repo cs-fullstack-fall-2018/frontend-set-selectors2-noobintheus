7fb9df2cad77c774c366d9f7ffebe2026db1fd39
Kenn <kenn+git@code-crew.org>
2018-08-15 16:37:12 -0500

    Graded 08-15-2018 16:35:46

--- js.js
+++ js.js
@@ -12,3 +12,16 @@ x.querySelector(".example").innerHTML = "Hello World!";
 
 document.querySelector("h1").style.backgroundColor = "blue";
 document.querySelector("goodbye").style.backgroundColor = "yellow";
+
+/*
+Question 1:
+	a) Not complete. The p tag is not called using ID, class, or tag selectors
+Questoin 2: 
+	a) Not complete. The special class is never called.
+Question 3:
+	a) Can be correct, but there's no HTML file in this project.
+Question 4:
+	a) Not complete. The last p tag is not callled.
+
+From Kenn
+*/
